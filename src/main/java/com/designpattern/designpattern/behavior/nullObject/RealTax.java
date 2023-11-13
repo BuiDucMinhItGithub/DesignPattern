230eb51052e1bd3a584c9c02dc5f4da57bc0f5c1
minh.buiduc <minh.buiduc@nttdata.com>
2023-11-13 17:45:09 +0700

    Null Object design pattern

---
+++ src/main/java/com/designpattern/designpattern/behavior/nullObject/RealTax.java
@@ -0,0 +1,22 @@
+package com.designpattern.designpattern.behavior.nullObject;
+
+public class RealTax implements Tax {
+  private String country;
+  private double vat;
+
+  public RealTax(String country, double vat) {
+    this.country = country;
+    this.vat = vat;
+  }
+
+  @Override
+  public String getCountry() {
+    return country;
+  }
+
+  @Override
+  public double apply(double price) {
+    return price * vat;
+  }
+
+}
